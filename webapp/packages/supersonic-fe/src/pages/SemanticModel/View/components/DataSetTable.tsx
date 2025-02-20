5c61eb4fb2a2520d1bde25233a99d7dabf3c7d38
williamhliu <williamhliu@tencent.com>
2025-02-20 08:09:33 +0800

    (feature)(chat-sdk) add upload file

--- webapp/packages/supersonic-fe/src/pages/SemanticModel/View/components/DataSetTable.tsx
+++ webapp/packages/supersonic-fe/src/pages/SemanticModel/View/components/DataSetTable.tsx
@@ -184,6 +184,12 @@ const DataSetTable: React.FC<Props> = ({ disabledEdit = false }) => {
                 启用
               </Button>
             )}
+            <UploadFile
+              key="uploadFile"
+              buttonType="link"
+              domainId={record.domainId}
+              datasetId={record.id}
+            />
             <Popconfirm
               title="确认删除？"
               okText="是"
@@ -224,6 +230,13 @@ const DataSetTable: React.FC<Props> = ({ disabledEdit = false }) => {
           disabledEdit
             ? [<></>]
             : [
+                <UploadFile
+                  key="uploadFile"
+                  domainId={selectDomainId}
+                  onFileUploaded={() => {
+                    queryDataSetList();
+                  }}
+                />,
                 <Button
                   key="create"
                   type="primary"
